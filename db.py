bf388602512724b10a5501eb0e87d834a252ed95
Naidmen12 <christianyesidcf@ufps.edu.co>
2025-10-27 23:14:05 -0500

    Update db.py

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
 
 # Railway (o cualquier PaaS) suele proporcionar DATABASE_URL en las env vars
 DATABASE_URL = os.getenv("DATABASE_URL")
